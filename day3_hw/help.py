e8bb1f189da6295b5f8ff74daa5087ef7cd9ccbe
Kittiguya <157630166+Kittiguya@users.noreply.github.com>
2024-02-29 14:49:00 -0600

    week 2 day 3 homework. I overcomplicate stuff so much.

---
+++ day3_hw/help.py
@@ -0,0 +1,20 @@
+def sq_ft(length, width):
+    """calulates sq ft of house"""
+    return  length * width
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def circle(diameter):
+    """Calculate circumference of a circle"""
+    return diameter * 3.14
